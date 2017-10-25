bfb8b8fcaa9b6a0d2eaa08b7f5815564d9a7b26d
Joel Lim <103879u@gmail.com>
2017-10-26 06:30:09 +0800

    update convert-to-midi.py

--- experiments/waon/convert-to-midi.py
+++ experiments/waon/convert-to-midi.py
@@ -1,4 +1,5 @@
 # use waon to convert wavs to midi
+# wav file will not be converted if midi is found
 
 import os
 from subprocess import call
@@ -6,14 +7,26 @@ from subprocess import call
 wavs_dir = '../wav-files/'
 
 for dir_name, dir_list, filelist in os.walk(wavs_dir):
-  for filename in filelist:
-    filepath = wavs_dir + filename
-    filepath = os.path.abspath(filepath)
-    filepath_wo_ext = filepath.replace('.wav', '')
-    filename_wo_ext = os.path.basename(filepath_wo_ext)
-    output_filepath = 'midis/' + filename_wo_ext + ".mid"
-    print "converting {} to {}".format(filepath, output_filepath)
-    print " ".join(["waon", "-i", filepath, "-o", output_filepath])
-    call(["waon", "-i", filepath, "-o", output_filepath])
-
-    
+    for filename in [filename for filename in filelist if '.wav' in filename]:
+        filepath = wavs_dir + filename
+        filepath = os.path.abspath(filepath)
+        filepath_wo_ext = filepath.replace('.wav', '')
+        filename_wo_ext = os.path.basename(filepath_wo_ext)
+        options_list = [
+            {'suffix': '', 'args': []},
+            {'suffix': '-b70-n8192',
+                'args': ["--bottom", "70", "-n", "8192"]},
+            {'suffix': '-b70-t90-n8192',
+                'args': ["--bottom", "70", "--top", "90", "-n", "8192"]},
+            {'suffix': '-b60-t70-8192',
+                'args': ["--bottom", "60", "--top", "70", "-n", "8192"]},
+            {'suffix': '-b50-t60-8192',
+                'args': ["--bottom", "50", "--top", "60", "-n", "8192"]}
+        ]
+        for options in options_list:
+            args = options["args"]
+            suffix = options['suffix']
+            output_filepath = 'midis/' + filename_wo_ext + " - " + suffix + ".mid"
+            if not os.path.isfile(output_filepath):
+                print "converting {} to {} with args {}".format(filepath, output_filepath, args)
+                call(["waon", "-i", filepath, "-o", output_filepath] + args)
