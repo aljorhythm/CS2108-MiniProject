15752e3fcb13158c26c979b4735c6ec543334252
Joel Lim <103879u@gmail.com>
2017-10-31 01:03:46 +0800

    extract midi properties from wav file
    Former-commit-id: cefaa2226ce5056976c558dde81dad021133eb60 [formerly 57b7ccf21e268476458d5928de2810828cbb571a] [formerly cefaa2226ce5056976c558dde81dad021133eb60 [formerly 57b7ccf21e268476458d5928de2810828cbb571a] [formerly cefaa2226ce5056976c558dde81dad021133eb60 [formerly 57b7ccf21e268476458d5928de2810828cbb571a] [formerly bfcf74e890d4852e6df087fe18f28fd14389b416 [formerly 02ded88f07b5588f9071faca74cf22426dbe1d5e]]]]
    Former-commit-id: 8e4cdafd4130e766d989e6a47f07a751347794d0
    Former-commit-id: 6b5ab1c0da3e08a9a5fe6c9bd4f31824252c4746 [formerly d850df2104d3269487f27a4056e61a2f98cb8f53] [formerly d5b5fb77b23bf379afb9733f375d8ad762045c17 [formerly c88fe7b76cb795a941b0b203234eeb7eeec3f49a]]
    Former-commit-id: 42c52fb5f998f0e0d99dc37ebf91d8a44e3e266e
    Former-commit-id: 1d78c9a77f0d15f964ece335265ff62bdbef38dc

---
+++ midi_properties.py
@@ -0,0 +1,136 @@
+import midi
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import argparse
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+diatonic_scale = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+keys = [np.roll(diatonic_scale, i) for i in range(0, 11)]
+
+# assign more weight to tonal root
+tonal_root_weight_addition = 2
+for index, key in enumerate(keys):
+  key[index] += tonal_root_weight_addition
+
+pitches = [
+            'c',
+            'c#',
+            'd',
+            'd#',
+            'e',
+            'f',
+            'f#',
+            'g',
+            'g#',
+            'a',
+            'a#',
+            'b'
+        ]
+
+class KeyUtils():
+    @staticmethod
+    def get_all_pitches():
+        global pitches
+        return pitches
+
+    @staticmethod
+    def get_keys():
+        global keys
+        return keys
+
+    @staticmethod
+    def print_keys():
+        keys = KeyUtils.get_keys()
+        pitches = KeyUtils.get_all_pitches()
+        print "key\tnotes"
+        for key_index in range(0, len(keys)):
+            key_letter = pitches[key_index]
+            pitches_numbers = filter(
+                lambda scale_degree: keys[key_index][scale_degree] == 1, range(len(keys[key_index])))
+            letters = [pitches[pitch_number]
+                       for pitch_number in pitches_numbers]
+            print key_letter + "\t" + ", ".join(letters)
+        print "-------------------------------\n"
+
+    @staticmethod
+    def get_absolute_pitch(pitch_number):
+        # https://newt.phys.unsw.edu.au/jw/notes.html
+        # C starts on 24
+        python_midi_pitch_offset = 26
+        return (pitch_number - python_midi_pitch_offset) % 11
+
+
+class MidiProperties():
+    def __init__(self, filename):
+        self.pattern = midi.read_midifile(filename)
+        self.filename = filename
+        self.pitch_counts = None
+        self.notes = None
+        self.sanitized_pitch_counts = None
+
+    def get_notes(self):
+        if self.notes is None:
+            self.notes = []
+
+            for track in self.pattern:
+                for event in track:
+                    # check that the current event is a NoteEvent, otherwise it won't have the method get_pitch() and we'll get an error
+                    if isinstance(event, midi.NoteEvent):
+                        pitch_number = event.get_pitch()
+                        reduced_pitch_number = KeyUtils.get_absolute_pitch(
+                            pitch_number)
+                        print pitch_number, reduced_pitch_number, KeyUtils.get_all_pitches()[reduced_pitch_number]
+                        self.notes.append(reduced_pitch_number)
+        return self.notes
+
+    def get_pitch_counts(self):
+        # count frequencies
+        if self.pitch_counts is None:
+            self.pitch_counts = np.zeros([len(KeyUtils.get_all_pitches())])
+
+            for reduced_pitch_number in self.get_notes():
+                self.pitch_counts[reduced_pitch_number] += 1
+
+        return self.pitch_counts
+
+    def get_notes_count(self):
+        return len(self.get_notes())
+
+    def get_sanitized_pitch_counts(self):
+        if self.sanitized_pitch_counts is None:
+
+            count_threshold_percentage = 0.05
+            count_threshold = count_threshold_percentage * self.get_notes_count()
+
+            self.sanitized_pitch_counts = [
+                0 if song_pitch_count < count_threshold else song_pitch_count for song_pitch, song_pitch_count in enumerate(self.get_pitch_counts())]
+
+        return self.sanitized_pitch_counts
+
+    def get_similar_keys(self):
+        return cosine_similarity([self.get_pitch_counts()], KeyUtils.get_keys())[0]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="Path to input midi file.")
+    args = parser.parse_args()
+
+    KeyUtils.print_keys()
+
+    midi_properties = MidiProperties(args.infile)
+
+    print "{}\t: {}".format("Pitches\t\t", "\t".join(KeyUtils.get_all_pitches()))
+    print "{}\t: {}".format("Pitch Counts\t", "\t".join([str(p) for p in midi_properties.get_pitch_counts()]))
+    print "{}\t: {}".format("Sanitized Counts", "\t".join([str(p) for p in midi_properties.get_sanitized_pitch_counts()]))
+    print "{}\t: {}".format("Sanitized pitches", "\t".join([KeyUtils.get_all_pitches()[pitch_number] if pitch_count > 0 else '-' for pitch_number, pitch_count in enumerate(midi_properties.get_sanitized_pitch_counts())]))
+
+    # find similarities
+
+    key_similarities = midi_properties.get_similar_keys()
+    print key_similarities
+    keys_sorted_indexes = np.argsort(key_similarities)
+    for key_index in np.flip(keys_sorted_indexes, 0):
+        print "{}\t: {}".format(pitches[key_index], key_similarities[key_index])
