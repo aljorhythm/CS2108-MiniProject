97c7e3af023574bb9bbffb9f6b2059217c88b71c
Joel Lim <103879u@gmail.com>
2017-11-01 15:28:37 +0800

    init libkeyfinder test
    Former-commit-id: 00076e1684439611ae18b12e8f1ac07054a15b5a [formerly f48843dcd76539e3e699de956ddecef24d4265dd] [formerly 754aed8a0cbdc3b8527e86512abef3775cfb53c2 [formerly 5d8f2d6e1ba7ac71f456cf84419367d96ad4b6ca]] [formerly 3919a4daf7e30243a3ae22d4addf6e323c7c893b [formerly 0f06477ce0650118f0219fc2bdc6b3c3d8f97685] [formerly 1712063f2a9e6b159c8d099175e9249b02a64e1c] [formerly 754aed8a0cbdc3b8527e86512abef3775cfb53c2 [formerly 5d8f2d6e1ba7ac71f456cf84419367d96ad4b6ca] [formerly d54b5f6f68e1fd4a21ef017deba410dac649698c [formerly 5a7998f48def331bfe8d1f71ca058bbdfa178267]]]]
    Former-commit-id: 8b0e828a0b8e8ec75dc17675f9df858b383ff73d
    Former-commit-id: fc47fcaaaef1b7666efe31d12206b8399ef19dc9 [formerly 30b989901f332c7829a224a2c6a3c9876f35f7c5] [formerly ba79bbc3e67a25802f7719a08acdc4c4fe7c63a1 [formerly b9ad51f9d9af1a44f6b9526c8fe83d9dde21160b]]
    Former-commit-id: e6d7a52c7f65a3df03c21c100d3e65a46c394b15
    Former-commit-id: dbe8dc69dafae69484cf21a30b85aea127aac9a0

--- test_libkeyfinder.py
+++ test_libkeyfinder.py
@@ -1,13 +1,11 @@
 import unittest
-from midi_properties import MidiProperties
-from midi_properties import KeyUtils
-
 import os
+import subprocess
 
 
-class TestMidiProperties(unittest.TestCase):
+class TestLibKeyFinder(unittest.TestCase):
 
-    def test_midi_properties(self):
+    def test(self):
         midi_dir = 'data/'
 
         if not os.path.exists(midi_dir):
@@ -15,23 +13,21 @@ class TestMidiProperties(unittest.TestCase):
         errors = []
         success = []
         for dir_name, dir_list, filelist in os.walk(midi_dir):
-            for filename in [filename for filename in filelist if filename.endswith('.mid')]:
+            for filename in [filename for filename in filelist if filename.endswith('.mp3')]:
                 filepath = midi_dir + filename
                 print "Checking {}".format(filepath)
                 expected_key = filename.split("-")[0]
-                midi_properties = MidiProperties(filepath)
-                key_sorted_indexes, key_similarities = midi_properties.get_similar_keys()
-                most_similar_key = KeyUtils.get_all_pitches()[
-                    key_sorted_indexes[0]]
+                most_similar_key = subprocess.check_output(['keyfinder-cli', filepath]).strip()
                 if most_similar_key != expected_key:
                     errors.append({
                         "filepath": filepath,
-                        "results": KeyUtils.pitch_numbers_to_letters(key_sorted_indexes)
+                        "expected": expected_key,
+                        "results": most_similar_key
                     })
                 else:
                     success.append(filepath)
         print "Correct Results:\n" + "\n".join(success)
-        msg = "\nWrong Results:\n" + "\n".join([err["filepath"] + "\n" + "\t".join(err["results"]) for err in errors])
+        msg = "\nWrong Results:\n" + "\n".join([err["filepath"] + ", expected " + err["expected"] + "\n" + err["results"] for err in errors])
         self.assertEqual(errors, [], msg = msg)
 
 if __name__ == '__main__':
