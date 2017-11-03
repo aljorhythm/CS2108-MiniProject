1f95161fc154a945cf00e09d254d874c50344b64
Jeremiah Ang <jeremiahang@Jeremiahs-MacBook-Pro.local>
2017-11-03 14:57:24 +0800

    Web interface to record and save mp3 file
    Former-commit-id: 13a5465a67a464601aa993f244f9a2c5b0a03cfb [formerly a05b51c357984bf50d6b45169b349824fec78bac] [formerly 9ed5428ad6c9111d42faedf57505e88ad226dfbd [formerly 71b10c566bb0dce3c3ff9cadfd54a3647f594633]] [formerly 24dd70684bbf68423e2087ff000eafb4ec6b35da [formerly cefc2d02fe60972fbf1f1bcab96b964845a8be36] [formerly a39257c9d9718c3ed25553aa349d13d217061d01] [formerly 9ed5428ad6c9111d42faedf57505e88ad226dfbd [formerly 71b10c566bb0dce3c3ff9cadfd54a3647f594633] [formerly f01a01f43d019f82aa740f22a13e6c0b85217c22 [formerly a4197ea7bb6cd8d40afb39080cae7069e85c6d21]]]]
    Former-commit-id: 24dd70684bbf68423e2087ff000eafb4ec6b35da [formerly cefc2d02fe60972fbf1f1bcab96b964845a8be36] [formerly a39257c9d9718c3ed25553aa349d13d217061d01]
    Former-commit-id: 24dd70684bbf68423e2087ff000eafb4ec6b35da [formerly cefc2d02fe60972fbf1f1bcab96b964845a8be36]
    Former-commit-id: a5dbef783d978713e2b71972fe05949054505b91
    Former-commit-id: 50ee912d3227182bae595d6e69d9926ce1d77133

---
+++ experiments/web-audio-recorder/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require ("path");
+module.exports = {
+	entry: "./app/build/index.js",
+	output: {
+		path: path.resolve(__dirname, "./app/dist/"),
+		filename: "webpack.bundle.js"
+	},
+	module: {
+		rules : [
+			{
+		        test: /\.Worker\.js$/,
+		        use: { 
+		        	loader: 'worker-loader',
+		        	options: { name: 'webWorker.worker.js' } 
+		        }
+		    }
+		]
+	}
+}
