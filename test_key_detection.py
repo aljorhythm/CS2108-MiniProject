1845622cf04d2f9812099ff07c5a4fb2ba4f7678
Joel Lim <103879u@gmail.com>
2017-11-05 22:15:25 +0800

    formatting changes
    Former-commit-id: 8ecb68f1b475a5d58227b4961119a0c6d4877b0d [formerly 033b4e664795bc1c03365b737362d9841c78d822] [formerly 8f1e71b3421460e43a18c8cfa473a03359234a78 [formerly 63a6a38b61a82affa3d4b80963bf3a6e1220f7d9]] [formerly f474fdffacdb9b391144a157b24cfca790b7e78a [formerly ee4dc9d7b0e4ecd9d81dd143e45307caf4c89fc9] [formerly e24b7ea33c92be061baeb42ae0d117959f8af658] [formerly 8f1e71b3421460e43a18c8cfa473a03359234a78 [formerly 63a6a38b61a82affa3d4b80963bf3a6e1220f7d9] [formerly 424d3bce171c923696fc53922e82469623e8f14d [formerly 95612124180a61c14a4987a0728cae76856200c0]]]]
    Former-commit-id: f474fdffacdb9b391144a157b24cfca790b7e78a [formerly ee4dc9d7b0e4ecd9d81dd143e45307caf4c89fc9] [formerly e24b7ea33c92be061baeb42ae0d117959f8af658]
    Former-commit-id: f474fdffacdb9b391144a157b24cfca790b7e78a [formerly ee4dc9d7b0e4ecd9d81dd143e45307caf4c89fc9]
    Former-commit-id: 3d030e81f794d7de726d88ede8f5884c9fe001c6
    Former-commit-id: 81e285de69f3a1dcf260748b446bdb363b76f078

--- test_key_detection.py
+++ test_key_detection.py
@@ -8,6 +8,8 @@ from midi_properties import KeyUtils
 class TestKeyDetection(unittest.TestCase):
 
     def test_libkeyfinder(self):
+        print "************************************"      
+        
         midi_dir = 'data/'
 
         if not os.path.exists(midi_dir):
@@ -17,24 +19,32 @@ class TestKeyDetection(unittest.TestCase):
         for dir_name, dir_list, filelist in os.walk(midi_dir):
             for filename in [filename for filename in filelist if filename.endswith('.mp3')]:
                 filepath = midi_dir + filename
-                print "Checking {}".format(filepath)
                 expected_key = filename.split("-")[0]
                 most_similar_key = subprocess.check_output(['keyfinder-cli', filepath]).strip()
                 if not KeyUtils.are_keys_equal(most_similar_key, expected_key):
                     errors.append({
                         "filepath": filepath,
-                        "expected": "'" + expected_key + "'",
-                        "results": "'" + most_similar_key + "'",
-                        "truth" : KeyUtils.are_keys_equal(most_similar_key, expected_key)
+                        "expected": expected_key,
+                        "results": KeyUtils.get_standard_key(most_similar_key)
                     })
                 else:
                     success.append(filepath)
+
+        total = len(success) + len(errors)
+        success_count = len(success)
+        percentage = success_count * 100.0 / total
+        print "LibKeyFinder: \t{} correct out of {}, {}%".format(len(success), total, percentage)
         print "Correct Results:\n" + "\n".join(success)
-        msg = "\nWrong Results:\n {}" .format("\n".join([err["filepath"] + " " + str(err["truth"]) + ", expected " + err["expected"] + "\n" + err["results"] for err in errors]))
+
+        msg = "\nWrong Results:\n{}" .format("\n".join(["{} :\t expected {},  \tactual {}".format(err["filepath"].ljust(40, ' '), err["expected"], err["results"]) for err in sorted(errors, key = lambda err: err["filepath"])]))
         self.assertEqual(errors, [], msg = msg)
+
+        print "************************************\n\n"
 
 
     def test_midi_properties(self):
+        print "************************************"    
+      
         midi_dir = 'data/'
 
         if not os.path.exists(midi_dir):
@@ -44,22 +54,33 @@ class TestKeyDetection(unittest.TestCase):
         for dir_name, dir_list, filelist in os.walk(midi_dir):
             for filename in [filename for filename in filelist if filename.endswith('.mid')]:
                 filepath = midi_dir + filename
-                print "Checking {}".format(filepath)
                 expected_key = filename.split("-")[0]
                 midi_properties = MidiProperties(filepath)
                 key_sorted_indexes, key_similarities = midi_properties.get_similar_keys()
                 most_similar_key = KeyUtils.get_all_pitches()[
                     key_sorted_indexes[0]]
                 if not KeyUtils.are_keys_equal(most_similar_key, expected_key):
+                    results = KeyUtils.pitch_numbers_to_letters(key_sorted_indexes)
                     errors.append({
                         "filepath": filepath,
-                        "results": KeyUtils.pitch_numbers_to_letters(key_sorted_indexes)
+                        "results": results,
+                        "incorrect_rank" : results.index(KeyUtils.get_standard_key(expected_key)) + 1,
+                        "expected_key": expected_key
                     })
                 else:
                     success.append(filepath)
+
+        total = len(success) + len(errors)
+        success_count = len(success)
+        percentage = success_count * 100.0 / total
+        print "MidiProperties:\t{} correct out of {}, {}%".format(success_count, total, percentage)
+
         print "Correct Results:\n" + "\n".join(success)
-        msg = "\nWrong Results:\n" + "\n".join([err["filepath"] + "\n" + "\t".join(err["results"]) for err in errors])
+        msg = "\nWrong Results:\n" + "\n".join(["{} Incorrect rank of {}\t: {},\t{}".format(err["filepath"].ljust(90), err["expected_key"], err["incorrect_rank"], "\t".join(err["results"])) for err in sorted(errors, key = lambda err: err["filepath"])])
+        
         self.assertEqual(errors, [], msg = msg)
+
+        print "************************************\n\n"
 
 if __name__ == '__main__':
     unittest.main()
