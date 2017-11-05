5434b4944b94cbe5c36e2750119676727e9d443a
Joel Lim <103879u@gmail.com>
2017-11-05 17:08:50 +0800

    refactor

--- test_midi_properties.py
+++ test_midi_properties.py
@@ -42,32 +42,5 @@ class TestMidiProperties(unittest.TestCase):
         for keypair in equal_keypairs:
           self.assertTrue(KeyUtils.are_keys_equal(keypair[0], keypair[1]), "{} != {}".format(keypair[0], keypair[1]))          
 
-    def test_midi_properties(self):
-        midi_dir = 'data/'
-
-        if not os.path.exists(midi_dir):
-            os.makedirs(midi_dir)
-        errors = []
-        success = []
-        for dir_name, dir_list, filelist in os.walk(midi_dir):
-            for filename in [filename for filename in filelist if filename.endswith('.mid')]:
-                filepath = midi_dir + filename
-                print "Checking {}".format(filepath)
-                expected_key = filename.split("-")[0]
-                midi_properties = MidiProperties(filepath)
-                key_sorted_indexes, key_similarities = midi_properties.get_similar_keys()
-                most_similar_key = KeyUtils.get_all_pitches()[
-                    key_sorted_indexes[0]]
-                if not KeyUtils.are_keys_equal(most_similar_key, expected_key):
-                    errors.append({
-                        "filepath": filepath,
-                        "results": KeyUtils.pitch_numbers_to_letters(key_sorted_indexes)
-                    })
-                else:
-                    success.append(filepath)
-        print "Correct Results:\n" + "\n".join(success)
-        msg = "\nWrong Results:\n" + "\n".join([err["filepath"] + "\n" + "\t".join(err["results"]) for err in errors])
-        self.assertEqual(errors, [], msg = msg)
-
 if __name__ == '__main__':
     unittest.main()
