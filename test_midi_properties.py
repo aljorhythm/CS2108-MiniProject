eb89ae9f1ab2550b780dd7a5c4e79f314529b4da
Joel Lim <103879u@gmail.com>
2017-11-05 17:53:32 +0800

    add tests
    Former-commit-id: 1938e25ab79182acf7a49053fd37f6d7c4a75b9a [formerly 00fdac47ab245d3736ecc38b342acaade13bf84b] [formerly 640cdc72b4ea179dcd0ea61c83b45507b837abee [formerly d718223241013f0911d27d696c9fb074955aeea4]] [formerly 8e3e9c8607e4ae131e7a3de741baa4128b4c2bf1 [formerly f45337516ba0d38eb0b2d3475fceacf83a1f22be] [formerly 1d293441b2e69ece565454a346cff33ad17e4df1] [formerly 640cdc72b4ea179dcd0ea61c83b45507b837abee [formerly d718223241013f0911d27d696c9fb074955aeea4] [formerly baea74c66a5749d48ec7352fbc5cc0fb0c3c94da [formerly 21db2b255b47f22e8fb6fcf7f67f9c7f0752f667]]]]
    Former-commit-id: 8e3e9c8607e4ae131e7a3de741baa4128b4c2bf1 [formerly f45337516ba0d38eb0b2d3475fceacf83a1f22be] [formerly 1d293441b2e69ece565454a346cff33ad17e4df1]
    Former-commit-id: 8e3e9c8607e4ae131e7a3de741baa4128b4c2bf1 [formerly f45337516ba0d38eb0b2d3475fceacf83a1f22be]
    Former-commit-id: 2c370a2f91bffa4b24be80cae90987552e843028
    Former-commit-id: dc1fce4589b7e6be38f8ab7b474a9cc1714781b5

--- test_midi_properties.py
+++ test_midi_properties.py
@@ -7,40 +7,66 @@ import os
 
 class TestMidiProperties(unittest.TestCase):
 
+    def test_flat_to_sharp(self):
+        pairs = [
+            ['Ab', 'G#'],
+            ['C', 'C'],
+            ['C#', 'C#'],
+            ['Bb', 'A#'],
+            ['Gb', 'F#'],
+            ['Eb', 'D#']
+        ]
+
+        for pair in pairs:
+            self.assertEqual(KeyUtils.flat_to_sharp(pair[0]), pair[1])
+
     def test_get_absolute_pitch(self):
         pairs = [
-          [24, 0],
-          [25, 1],
-          [60, 0],
-          [69, 9],
-          [95, 11]
+            [24, 0],
+            [25, 1],
+            [60, 0],
+            [69, 9],
+            [95, 11]
         ]
         for pair in pairs:
-          midi_note = pair[0]
-          absolute_number = pair[1]
-          self.assertEqual(KeyUtils.get_absolute_pitch(midi_note), absolute_number)
+            midi_note = pair[0]
+            absolute_number = pair[1]
+            converted = KeyUtils.get_absolute_pitch(midi_note)
+            self.assertEqual(converted, absolute_number, "MIDI {}, CONVERTED {}, EXPECTED {}".format(
+                midi_note, converted, absolute_number))
 
     def test_get_standard_key(self):
-        self.assertEqual(KeyUtils.get_standard_key('Am'), 'C')
-        self.assertEqual(KeyUtils.get_standard_key('Bm'), 'D')
-        self.assertEqual(KeyUtils.get_standard_key('Cm'), 'D#')
-        self.assertEqual(KeyUtils.get_standard_key('Dm'), 'F')
-        self.assertEqual(KeyUtils.get_standard_key('Em'), 'G')
-        self.assertEqual(KeyUtils.get_standard_key('Eb'), 'D#')
-        self.assertEqual(KeyUtils.get_standard_key('Eb'), 'D#')
+        pairs = [
+            ['Am', 'C'],
+            ['Bm', 'D'],
+            ['Cm', 'D#'],
+            ['Eb', 'D#'],
+            ['Dm', 'F'],
+            ['Em', 'G'],
+            ['Eb', 'D#'],
+            ['Abm', "B"]
+        ]
+
+        for pair in pairs:
+            result = KeyUtils.get_standard_key(pair[0])
+            self.assertEqual(
+                result, pair[1], "Expected {} \t, actual {}".format(pair[1], result))
 
     def test_are_keys_equal(self):
         equal_keypairs = [
-          ['Am', 'C'],
-          ['Bm', 'D'],
-          ['Cm', 'Eb'],
-          ['Eb', 'Eb'],
-          ['Dm', 'F'],
-          ['Em', 'G']
+            ['Am', 'C'],
+            ['Bm', 'D'],
+            ['Cm', 'Eb'],
+            ['Eb', 'Eb'],
+            ['Dm', 'F'],
+            ['Em', 'G'],
+            ['Abm', "B"]
         ]
 
         for keypair in equal_keypairs:
-          self.assertTrue(KeyUtils.are_keys_equal(keypair[0], keypair[1]), "{} != {}".format(keypair[0], keypair[1]))          
+            self.assertTrue(KeyUtils.are_keys_equal(
+                keypair[0], keypair[1]), "{} != {}".format(keypair[0], keypair[1]))
+
 
 if __name__ == '__main__':
     unittest.main()
