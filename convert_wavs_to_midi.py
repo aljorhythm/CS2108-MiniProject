b701b5dc21ca15bbbfd17557417bca95736c2969
Joel Lim <103879u@gmail.com>
2017-11-05 22:09:35 +0800

    merge waon and melodia conversions
    Former-commit-id: 4c8b4758bd92a3fd119477779703624abeed0f3c [formerly 4faed726d7bec9b56d0023811d2a09205679152c] [formerly 2766d96b752f641db876c2603372faf3439f7c88 [formerly 476a52ecfb69316e9179e066d267d94f0afecfb4]] [formerly d7234d7bb50ac7e9e9dbc56a462a85fe8b2e2b8f [formerly f844c9a19228210dada20f7257eb1abbb6112e37] [formerly 7fefcdc7664c76ce9d38aaecee312f30a3654dcf] [formerly 2766d96b752f641db876c2603372faf3439f7c88 [formerly 476a52ecfb69316e9179e066d267d94f0afecfb4] [formerly 0d98c810013cd19eeab3c5110b66b54ae64eb83c [formerly b2845972fa1e3d5968354d26c013b5e0e1e2fedc]]]]
    Former-commit-id: 2e0d81892f4b8e22b9349492c697153bbf19b1a7
    Former-commit-id: b6cd3c5338e1d981ddebad4dca1869855cb75b91 [formerly 3690af600f5440c4b4d6106795a08431c11d6be2] [formerly 36efd3e5552e508f1ba7bb14a777b035c1240998 [formerly 6a03963bdb8342a103d3155ee3762961bc3cbfbf]]
    Former-commit-id: d6b208252aa76f2779afc5897e84b6ddbf562ce3
    Former-commit-id: 4518d93675144536a218f7be3349035175e8f186

--- convert_wavs_to_midi.py
+++ convert_wavs_to_midi.py
@@ -13,6 +13,8 @@ def audio_to_bpm(infile):
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
     return int(tempo)
 
+# use melodia
+
 if not os.path.exists(midi_dir):
     os.makedirs(midi_dir)
 
@@ -41,3 +43,30 @@ for dir_name, dir_list, filelist in os.walk(wavs_dir):
                 print "converting {} to {} with args {}".format(filepath, output_filepath, args)
                 bpm = audio_to_bpm(filepath)
                 audio_to_midi_melodia(filepath, output_filepath, bpm)
+
+# use waon
+
+for dir_name, dir_list, filelist in os.walk(wavs_dir):
+    for filename in [filename for filename in filelist if '.wav' in filename]:
+        filepath = wavs_dir + filename
+        filepath = os.path.abspath(filepath)
+        filepath_wo_ext = filepath.replace('.wav', '')
+        filename_wo_ext = os.path.basename(filepath_wo_ext)
+        options_list = [
+            {'suffix': '', 'args': []},
+            # {'suffix': '-b70-n8192',
+            #     'args': ["--bottom", "70", "-n", "8192"]},
+            # {'suffix': '-b70-t90-n8192',
+            #     'args': ["--bottom", "70", "--top", "90", "-n", "8192"]},
+            # {'suffix': '-b60-t70-8192',
+            #     'args': ["--bottom", "60", "--top", "70", "-n", "8192"]},
+            # {'suffix': '-b50-t60-8192',
+            #     'args': ["--bottom", "50", "--top", "60", "-n", "8192"]}
+        ]
+        for options in options_list:
+            args = options["args"]
+            suffix = options['suffix']
+            output_filepath = "{}{}{}.{}".format(midi_dir, filename_wo_ext, "_waon" + ("_" + suffix if suffix != "" else ""), "mid")
+            if not os.path.isfile(output_filepath):
+                print "converting {} to {} with args {}".format(filepath, output_filepath, args)
+                call(["waon", "-i", filepath, "-o", output_filepath] + args)
