2bdcca1c472fce48b02f8fbc7afe72903b8a0d92
Joel Lim <103879u@gmail.com>
2017-11-12 04:45:42 +0800

    edit download script
    Former-commit-id: 0301138a2e11acced17b6def1be82cdbea3cc6eb
    Former-commit-id: 6774657c47b0630783c3f47b7552970590c54ca0
    Former-commit-id: 1110a5ce142749003471d7d8059500600242690f [formerly d05a6a85aafe5873eb2951e7ed9e6ef80edbf03a]
    Former-commit-id: 8599906e8d73f2a9de8f01f8f35ac33f00afb087
    Former-commit-id: 37a188306d907f43ecc3004e63f62edba32d1f66

--- experiments/download-yt-wavs/download-yt-wavs.py
+++ experiments/download-yt-wavs/download-yt-wavs.py
@@ -2,19 +2,27 @@
 
 from subprocess import call
 
-wav_dir = "../wav-files/"
+wav_dir = "../data/"
 
 youtube_links = [{
-    "link": "https://www.youtube.com/watch?v=LRP8d7hhpoQ"
+    "link": "https://www.youtube.com/watch?v=LRP8d7hhpoQ",
+    "name" : "A#-hallelujah-pentatonix"
 }, {
-    "link": "https://www.youtube.com/watch?v=lHjkix9oKIE"
+    "link": "https://www.youtube.com/watch?v=XvVmZmMLojc",
+    "name": "Em-Somebody_That_I_Used_To_Know_vocals"
+}, {
+    "link": "https://www.youtube.com/watch?v=byXb3KT1-3A",
+    "name": "D-ThinkingOutLoud_vocals"
+},{
+  "link" : "https://www.youtube.com/watch?v=XMqEFuGA2cE",
+  "name" : "G#-Perfect_vocals"
 }]
 
 for youtube_link in youtube_links:
     print "downloading " + youtube_link["link"]
-    call(["youtube-dl", youtube_link["link"], 
-    	"-o", 
-    	"".join((wav_dir, "%(title)s.%(ext)s")), 
-    	"--extract-audio", 
-    	"--audio-format", 
-    	"wav"])
+    call(["youtube-dl", youtube_link["link"],
+          "-o",
+          "".join((wav_dir, "{}.%(ext)s".format(youtube_link["name"]))),
+          "--extract-audio",
+          "--audio-format",
+          "wav"])
