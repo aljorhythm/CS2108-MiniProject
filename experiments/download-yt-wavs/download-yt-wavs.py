162bd4661f53e4564d80dfafc74ea19575294cd2
Joel Lim <103879u@gmail.com>
2017-11-12 06:11:27 +0800

    add download files
    Former-commit-id: 2a26a1633b6d70baaa729b40b9a3ba2820dd8261
    Former-commit-id: 8f4ef22d51b9878921bb1010698a223bc6d38d1d
    Former-commit-id: 5c0727a71a4f1c8bda7ba055a2e2444320f848b6 [formerly 201a72aacacc4854a0fc47452a5a111f66fa8f9e]
    Former-commit-id: a04081b42d25ad2f65e311976a0f87a88eb2a3b6
    Former-commit-id: 97f614a6c758885292d164012456f12106ab9708

--- experiments/download-yt-wavs/download-yt-wavs.py
+++ experiments/download-yt-wavs/download-yt-wavs.py
@@ -2,27 +2,73 @@
 
 from subprocess import call
 
-wav_dir = "../data/"
+wav_dir = "../../data/"
 
 youtube_links = [{
     "link": "https://www.youtube.com/watch?v=LRP8d7hhpoQ",
-    "name" : "A#-hallelujah-pentatonix"
+    "name": "A#-hallelujah-pentatonix",
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=XvVmZmMLojc",
-    "name": "Em-Somebody_That_I_Used_To_Know_vocals"
+    "name": "Em-Somebody_That_I_Used_To_Know_vocals",
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=byXb3KT1-3A",
-    "name": "D-ThinkingOutLoud_vocals"
-},{
-  "link" : "https://www.youtube.com/watch?v=XMqEFuGA2cE",
-  "name" : "G#-Perfect_vocals"
+    "name": "D-ThinkingOutLoud_vocals",
+    "downloaded": True
+}, {
+    "link": "https://www.youtube.com/watch?v=XMqEFuGA2cE",
+    "name": "G#-Perfect_vocals",
+    "downloaded": True
+},  {
+    "link": "https://www.youtube.com/watch?v=yxaA94HGo9A",
+    "name": "G-pengyou",
+    "downloaded": True
+},
+    {
+    "link": "https://www.youtube.com/watch?v=VuA86Lv-bx0",
+    "name": "C-xiaowei",
+    "downloaded": True
+}, {
+    "link": "https://www.youtube.com/watch?v=UprcpdwuwCg",
+    "name": "G-heathens_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=Y7XWElxq0Io",
+    "name": "G-heathens_vocals_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=EFEmTsfFL5A",
+    "name": "E-aint_it_fun_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=k4V3Mo61fJM",
+    "name": "D#-fix_you_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=7Bz2E8KxedM",
+    "name": "D#-fix_you_vocals_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=Ho32Oh6b4jc",
+    "name": "D-perfect_one_direction_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=3aLjoFzM7IU",
+    "name": "A-Wonderwall_vocals_original",
+    "downloaded": False
+}, {
+    "link": "https://www.youtube.com/watch?v=bx1Bh8ZvH84",
+    "name": "A-wonderwall_original",
+    "downloaded" : False
 }]
 
 for youtube_link in youtube_links:
-    print "downloading " + youtube_link["link"]
-    call(["youtube-dl", youtube_link["link"],
-          "-o",
-          "".join((wav_dir, "{}.%(ext)s".format(youtube_link["name"]))),
-          "--extract-audio",
-          "--audio-format",
-          "wav"])
+    if not youtube_link["downloaded"]:
+        print "downloading " + youtube_link["link"]
+        call(["youtube-dl", youtube_link["link"],
+              "-o",
+              "".join((wav_dir, "{}.%(ext)s".format(youtube_link["name"]))),
+              "--extract-audio",
+              "--audio-format",
+              "wav"])
