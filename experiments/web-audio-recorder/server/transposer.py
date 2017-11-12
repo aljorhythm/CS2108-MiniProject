30dc22943e1f78a80153d46f0ceebca9a0a0760a
Joel Lim <103879u@gmail.com>
2017-11-12 19:36:08 +0800

    add convert to midi
    Former-commit-id: 2e14c9cb4620bfe79484ec16010be422f1ea8e13
    Former-commit-id: c4535f6cffb9e0dae1d0c293ae5f7395a57da412
    Former-commit-id: a1e85e34df335c7848d986467d75dace3c490ab3 [formerly 47d4a23c2efe93d862e59252fb0a8fc7679af7b1] [formerly c6652baa259fbe5d74a93082194de5b06231ed80 [formerly ef2aa7f8d6a9cec30ecb884b4174304f3a4ff4d0]]
    Former-commit-id: 4cee342b1fdef9ff5105a2ac67b61129ed0c2c07
    Former-commit-id: 209ef2473a3458792c177398dfaedcf48691cf29

--- experiments/web-audio-recorder/server/transposer.py
+++ experiments/web-audio-recorder/server/transposer.py
@@ -1,14 +1,35 @@
 import transposerutils as utils
 
+import os
+import librosa
+from audio_to_midi_melodia import audio_to_midi_melodia
+from subprocess import call
+
 def songlist():
 	return utils.songlist()
 
+def convert_to_midi(filepath):
+	filepath = os.path.abspath(filepath)
+	filepath_wo_ext = filepath.replace('.wav', '')
+	filename_wo_ext = os.path.basename(filepath_wo_ext)
+	options = {'suffix': '', 'args': []}
+	args = options["args"]
+	suffix = options['suffix']
+	output_filepath = "{}{}{}.{}".format(midi_dir, filename_wo_ext, "_melodia" + ("_" + suffix if suffix != "" else ""), "mid")
+	if not os.path.isfile(output_filepath):
+		print "converting {} to {} with args {}".format(filepath, output_filepath, args)
+		bpm = audio_to_bpm(filepath)
+		audio_to_midi_melodia(filepath, output_filepath, bpm)
+	return output_filepath
+	
 def process_recording (title, author, data):
 	recording_path = "./tmp/file.wav"
 	songs_path = "./src/songs"
 	original_path = utils.findsongpath(songs_path, title, author);
 	utils.writeWavFile(recording_path, data)
-	output_path, key = utils.analyseandtranspose(recording_path, original_path)
+
+	recording_midi = convert_to_midi(original_path)
+	output_path, key = utils.analyseandtranspose(recording_midi, original_path)
 
 	return output_path, key
 
