7ff8ec57a0a1b4157aa61469d36bdd63cf610b6a
Joel Lim <103879u@gmail.com>
2017-11-12 17:07:03 +0800

    update download create script
    Former-commit-id: 0b6e2b094317fda82d73f4077fa2bec9d597a6e2
    Former-commit-id: eddb9eb685aa31a97be371b34bfb04d97f3f1d0d
    Former-commit-id: 8539f6c4b93112a19e88d2a5871a7bedf9b8ca1c [formerly 032bc41f3c365c8dd16b0d1434e83c7afacab520]
    Former-commit-id: ae46cee744695be4167870ef6a6075bb2ba58fdb
    Former-commit-id: 5b0063ce330d504860ec6c5a6aa13c5afd3daaeb

--- download-yt-wavs.py
+++ download-yt-wavs.py
@@ -2,7 +2,7 @@
 
 from subprocess import call
 
-wav_dir = "../../data/"
+wav_dir = "data/"
 
 youtube_links = [{
     "link": "https://www.youtube.com/watch?v=LRP8d7hhpoQ",
@@ -15,6 +15,11 @@ youtube_links = [{
 }, {
     "link": "https://www.youtube.com/watch?v=byXb3KT1-3A",
     "name": "D-ThinkingOutLoud_vocals",
+    "downloaded": True
+},
+{
+    "link": "https://www.youtube.com/watch?v=qlCdmVnZzqc",
+    "name": "D-ThinkingOutLoud_original",
     "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=XMqEFuGA2cE",
@@ -32,35 +37,35 @@ youtube_links = [{
 }, {
     "link": "https://www.youtube.com/watch?v=UprcpdwuwCg",
     "name": "G-heathens_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=Y7XWElxq0Io",
     "name": "G-heathens_vocals_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=EFEmTsfFL5A",
     "name": "E-aint_it_fun_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=k4V3Mo61fJM",
     "name": "D#-fix_you_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=7Bz2E8KxedM",
     "name": "D#-fix_you_vocals_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=Ho32Oh6b4jc",
     "name": "D-perfect_one_direction_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=3aLjoFzM7IU",
     "name": "A-Wonderwall_vocals_original",
-    "downloaded": False
+    "downloaded": True
 }, {
     "link": "https://www.youtube.com/watch?v=bx1Bh8ZvH84",
     "name": "A-wonderwall_original",
-    "downloaded" : False
+    "downloaded" : True
 }]
 
 for youtube_link in youtube_links:
